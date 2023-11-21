6841f9936ba6d1fe31fe52da647ccccbfc734183
riraonline <lyra.online125@gmail.com>
2023-11-21 18:00:02 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']) -> name('homepage');
+Route::get('contact', [\App\Http\Controllers\HomeController::class, 'contact']) -> name('contact');
+Route::get('detail', [\App\Http\Controllers\HomeController::class, 'detail']) -> name('detail');
+
+Route::get('admin/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']) -> name('admin.dashboard.index');
